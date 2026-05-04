833ade35c349d0b155cefa6d9b9106b4579e6a42
Bhupendra Singh Negi <bhupendrasinghnegi11@gmail.com>
2026-05-04 13:37:44 +0530

    02_DynamicNestedRoute_RouteGroup_PrivateFolder

---
+++ module2/app/(auth)/sign-in/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SignInForm from './_components/sign-in-form'
+const SignInPage = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <hr/>
+      <SignInForm />
+      <hr/>
+      <h2>This is the sign in page</h2>      
+    </div>
+  )
+}
+
+export default SignInPage
